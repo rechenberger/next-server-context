bd0e2910366240b9d74c4d48f2c3bd51fa63cde1
Tristan Rechenberger <rechenberger@me.com>
2024-05-17 11:33:02 +0200

    ⚗️ api

--- apps/demo/src/pages/api/hello.ts
+++ apps/demo/src/pages/api/hello.ts
@@ -1,7 +1,19 @@
+import { createServerContext } from '@sodefa/next-server-context'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('OK')
+  a()
+  b()
 
   res.status(200).json({ message: 'Hello from Next.js!' })
+}
+
+const ctx = createServerContext()
+
+const a = async () => {
+  ctx.set(true)
+}
+
+const b = async () => {
+  console.log(!!ctx.get())
 }
